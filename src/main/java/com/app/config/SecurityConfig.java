6c6606647aabdf492dd4c71bfc88f6a9148c883d
daniescobarx <daniela.mollo@sptech.school>
2024-05-24 11:33:10 -0300

    Initial commit

---
+++ src/main/java/com/app/config/SecurityConfig.java
@@ -0,0 +1,88 @@
+package com.app.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    //condiciones de seguridad
+    //stateless - va depender del tiempo del token
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf(csrf -> csrf.disable())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(http -> {
+                    http.requestMatchers(HttpMethod.GET, "/auth/hello").permitAll();
+                    http.requestMatchers(HttpMethod.GET, "/auth/hello-secured").hasAuthority("CREATE");
+                    http.anyRequest().denyAll();
+                })
+                .build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    //user from db
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userDetailsService());
+        return provider;
+    }
+
+    //user
+    @Bean
+    public UserDetailsService userDetailsService(){
+        List<UserDetails> userDetailsList = new ArrayList<>();
+
+        userDetailsList.add(User.withUsername("daniela")
+                .password("123456")
+                .roles("ADMIN")
+                .authorities("READ", "CREATE")
+                .build());
+
+        userDetailsList.add(User.withUsername("santiago")
+                .password("123456")
+                .roles("USER")
+                .authorities("READ")
+                .build());
+
+        return new InMemoryUserDetailsManager(userDetailsList);
+
+    }
+    //password
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+}
---
+++ src/main/java/com/app/controller/TestAuthController.java
@@ -0,0 +1,23 @@
+package com.app.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/auth")
+public class TestAuthController {
+
+    @GetMapping("/hello")
+    public String hello(){
+        return "oi pessoal";
+    }
+
+    @GetMapping("/hello-secured")
+    public String helloSecured(){
+        return "oi pessoal teste secured";
+    }
+
+}

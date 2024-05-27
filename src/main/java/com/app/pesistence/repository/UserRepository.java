f1d3353a3c2cc6258a060cdb4ba1e9d284c0fb28
daniescobarx <daniela.mollo@sptech.school>
2024-05-27 13:11:51 -0300

    add service

--- src/main/java/com/app/pesistence/repository/UserRepository.java
+++ src/main/java/com/app/pesistence/repository/UserRepository.java
@@ -1,10 +1,17 @@
 package com.app.pesistence.repository;
 
 import com.app.pesistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long> {
 
+    Optional<UserEntity> findUserEntityByUsername(String username);
+
+    @Query("SELECT u FROM UserEntity u WHERE u.username = ?")
+    Optional<UserEntity> findUser(String username);
 }

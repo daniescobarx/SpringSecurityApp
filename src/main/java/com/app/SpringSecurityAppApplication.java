c3fa5952cea7d22379f32dfdedbd7b5641e77773
daniescobarx <daniela.mollo@sptech.school>
2024-05-27 00:36:46 -0300

    att

--- src/main/java/com/app/SpringSecurityAppApplication.java
+++ src/main/java/com/app/SpringSecurityAppApplication.java
@@ -1,7 +1,11 @@
 package com.app;
 
+import com.app.pesistence.entity.PermissionEntity;
+import com.app.pesistence.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringSecurityAppApplication {
@@ -10,4 +14,12 @@ public class SpringSecurityAppApplication {
 		SpringApplication.run(SpringSecurityAppApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner init(UserRepository userRepository){
+		return args -> {
+			PermissionEntity createPermission = PermissionEntity.builder()
+					.name("CREATE")
+					.build();
+		};
+	}
 }

a5ed9dd71ecbdd713d83dafdac4dab9509a66ca5
daniescobarx <daniela.mollo@sptech.school>
2024-05-27 12:16:52 -0300

    roles, users

--- src/main/java/com/app/SpringSecurityAppApplication.java
+++ src/main/java/com/app/SpringSecurityAppApplication.java
@@ -1,11 +1,16 @@
 package com.app;
 
 import com.app.pesistence.entity.PermissionEntity;
+import com.app.pesistence.entity.RoleEntity;
+import com.app.pesistence.entity.RoleEnum;
+import com.app.pesistence.entity.UserEntity;
 import com.app.pesistence.repository.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+import java.util.Set;
 
 @SpringBootApplication
 public class SpringSecurityAppApplication {
@@ -17,6 +22,8 @@ public class SpringSecurityAppApplication {
 	@Bean
 	CommandLineRunner init(UserRepository userRepository){
 		return args -> {
+
+			//permissions
 			PermissionEntity createPermission = PermissionEntity.builder()
 					.name("CREATE")
 					.build();
@@ -37,7 +44,67 @@ public class SpringSecurityAppApplication {
 					.name("REFACTOR")
 					.build();
 
+			//roles
+			RoleEntity roleAdmin = RoleEntity.builder()
+					.roleEnum(RoleEnum.ADMIN)
+					.permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission))
+					.build();
 
+			RoleEntity roleUser = RoleEntity.builder()
+					.roleEnum(RoleEnum.USER)
+					.permissionList(Set.of(createPermission, readPermission))
+					.build();
+
+			RoleEntity roleInvited = RoleEntity.builder()
+					.roleEnum(RoleEnum.INVITED)
+					.permissionList(Set.of(readPermission))
+					.build();
+
+			RoleEntity roleDeveloper = RoleEntity.builder()
+					.roleEnum(RoleEnum.DEVELOPER)
+					.permissionList(Set.of(createPermission, readPermission, updatePermission, deletePermission, refactorPermission))
+					.build();
+
+			//users
+			UserEntity userDaniela = UserEntity.builder()
+					.username("daniela")
+					.password("123")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialNoExpired(true)
+					.roles(Set.of(roleAdmin))
+					.build();
+
+			UserEntity userAndy = UserEntity.builder()
+					.username("andy")
+					.password("123")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialNoExpired(true)
+					.roles(Set.of(roleUser))
+					.build();
+
+			UserEntity userFredy = UserEntity.builder()
+					.username("fredy")
+					.password("123")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialNoExpired(true)
+					.roles(Set.of(roleInvited))
+					.build();
+
+			UserEntity userPaula = UserEntity.builder()
+					.username("paula")
+					.password("123")
+					.isEnabled(true)
+					.accountNoExpired(true)
+					.accountNoLocked(true)
+					.credentialNoExpired(true)
+					.roles(Set.of(roleDeveloper))
+					.build();
 
 		};
 	}
